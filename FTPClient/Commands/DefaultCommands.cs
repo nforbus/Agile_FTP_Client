4ac75746dc6805cdd93e1e82f424d8bce6d3c449
shraddha bhise <shraddhabhise838@gmail.com>
2019-03-09 11:04:24 -0800

    Resolving conflicts

--- FTPClient/Commands/DefaultCommands.cs
+++ FTPClient/Commands/DefaultCommands.cs
@@ -1,6 +1,11 @@
 using System;
+<<<<<<< Updated upstream
 using System.Net;
 using System.Security.Cryptography;
+=======
+using System.IO;
+using System.Linq;
+>>>>>>> Stashed changes
 using FluentFTP;
 
 namespace FTPClient.Commands
@@ -49,10 +54,40 @@ namespace FTPClient.Commands
                 returnMessage = "Connection failed with Exception";
             }
 
+<<<<<<< Updated upstream
             return returnMessage;
         }
 
         public static string listLocalDir(string path)
+=======
+        public static string Login(string address)
+        {
+            string returnMessage = "";
+            try
+            {
+                FtpClient client = new FtpClient(address);
+
+                client.Connect();
+
+                if (client.IsConnected)
+                {
+                    Client.serverName = address;
+                    Client.clientObject = client;
+                    Client.viewingRemote = true;
+
+                    returnMessage = "Connected to " + address;
+                }
+            }
+            catch (Exception e)
+            {
+                returnMessage = "Connection failed with Exception";
+            }
+
+            return returnMessage;
+        }
+
+        public static string ls(string path)
+>>>>>>> Stashed changes
         {
             string returnMessage = "";
             try
@@ -68,6 +103,31 @@ namespace FTPClient.Commands
             {
                 returnMessage = "Listing of local directories failed with Exception";
             }
+<<<<<<< Updated upstream
+=======
+            return returnMessage;
+        }
+        public static string findl(string path)
+        {
+            string returnMessage = "";
+            try
+            {
+                string curFile = @path;
+                if (File.Exists(curFile))
+                {
+                    returnMessage = "File exists.";
+                }
+                else
+                {
+                    returnMessage = "File does not exist.";
+                }
+
+            }
+            catch (Exception e)
+            {
+                returnMessage = "Failed with Exception";
+            }
+>>>>>>> Stashed changes
             return returnMessage;
         }
     }
