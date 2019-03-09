a7e9772e974e166a36a041c8ea26f15ca5b1a666
shraddha bhise <shraddhabhise838@gmail.com>
2019-03-09 13:27:52 -0800

    Changes for list local

--- FTPClient/Commands/DefaultCommands.cs
+++ FTPClient/Commands/DefaultCommands.cs
@@ -1,19 +1,16 @@
 using System;
-<<<<<<< Updated upstream
 using System.Net;
 using System.Security.Cryptography;
-=======
 using System.IO;
 using System.Linq;
->>>>>>> Stashed changes
 using FluentFTP;
 
 namespace FTPClient.Commands
 {
     public static class DefaultCommands
     {
-        
-        public static string Login(string address, string username="")
+
+        public static string Login(string address, string username = "")
         {
             string returnMessage = "";
             string password = "";
@@ -27,12 +24,12 @@ namespace FTPClient.Commands
                 System.Console.Write("Enter the password: ");
                 password = FTPClient.Console.Console.ReadPassword();
                 System.Console.Write('\n');
-                
+
             }
-            
+
             try
             {
-                
+
                 FtpClient client = new FtpClient(address)
                 {
                     Credentials = new NetworkCredential(username, password)
@@ -54,40 +51,10 @@ namespace FTPClient.Commands
                 returnMessage = "Connection failed with Exception";
             }
 
-<<<<<<< Updated upstream
-            return returnMessage;
-        }
-
-        public static string listLocalDir(string path)
-=======
-        public static string Login(string address)
-        {
-            string returnMessage = "";
-            try
-            {
-                FtpClient client = new FtpClient(address);
-
-                client.Connect();
-
-                if (client.IsConnected)
-                {
-                    Client.serverName = address;
-                    Client.clientObject = client;
-                    Client.viewingRemote = true;
-
-                    returnMessage = "Connected to " + address;
-                }
-            }
-            catch (Exception e)
-            {
-                returnMessage = "Connection failed with Exception";
-            }
-
             return returnMessage;
         }
 
         public static string ls(string path)
->>>>>>> Stashed changes
         {
             string returnMessage = "";
             try
@@ -103,33 +70,9 @@ namespace FTPClient.Commands
             {
                 returnMessage = "Listing of local directories failed with Exception";
             }
-<<<<<<< Updated upstream
-=======
             return returnMessage;
         }
-        public static string findl(string path)
-        {
-            string returnMessage = "";
-            try
-            {
-                string curFile = @path;
-                if (File.Exists(curFile))
-                {
-                    returnMessage = "File exists.";
-                }
-                else
-                {
-                    returnMessage = "File does not exist.";
-                }
 
-            }
-            catch (Exception e)
-            {
-                returnMessage = "Failed with Exception";
-            }
->>>>>>> Stashed changes
-            return returnMessage;
-        }
     }
 }
     
