9ec7f9b2a11e2d3a6dabe316aa1b5ffc99227990
Nikk Forbus <nforbus@pdx.edu>
2019-03-09 12:53:32 -0800

    Added and tested rnRemote.

--- FTPClient/Commands/DefaultCommands.cs
+++ FTPClient/Commands/DefaultCommands.cs
@@ -63,5 +63,81 @@ namespace FTPClient.Commands
         {
             return FTPClient.Client.clientObject.GetWorkingDirectory();
         }
-     }
+
+        public static string listRemote()
+        {
+            string returnMessage = "";
+            string res = "";
+            try
+            {
+                foreach (FtpListItem item in Client.clientObject.GetListing("/"))
+                {
+                    res += item.FullName + "\n";
+                }
+                returnMessage = res;
+            }
+            catch (Exception e)
+            {
+                returnMessage = "Listing failed with Exception";
+            }
+            return returnMessage;
+        }
+
+        public static string listLocal(string path)
+        {
+            string returnMessage = "";
+            try
+            {
+                returnMessage = "The directories are:" + "\n";
+                var directories = System.IO.Directory.GetDirectories(path);
+                foreach (string item in directories)
+                {
+                    returnMessage = returnMessage + '\n' + item;
+                }
+            }
+            catch (Exception e)
+            {
+                returnMessage = "Listing of local directories failed with exception";
+            }
+            return returnMessage;
+        }
+
+        public static string rnRemote(string target, string newName)
+        {
+            string returnMessage = "";
+            try
+            {
+                foreach (FtpListItem item in Client.clientObject.GetListing("/"))
+                {
+                    if (item.FullName == target)
+                    {
+                        Client.clientObject.Rename(target, newName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                returnMessage = "File could not be renamed.";
+            }
+            return returnMessage;
+        }
+
+        /*
+        public static string showPerms(string path)
+        {
+            string returnMessage = "";
+            int permissions;
+            try
+            {
+                permissions = Client.clientObject.GetChmod(path);
+                Console.Console.WriteToConsole("permissions are: " + permissions);
+            }
+            catch (Exception e)
+            {
+                returnMessage = "Couldnt check the permissions.";
+            }
+            return returnMessage;
+        }
+        */
+    }
 }
